d927b29130358a7211048d7597ad7486c57c557f
Feather_red <1328181376@qq.com>
2024-03-26 17:47:28 +0800

    实现七类文献类，并生成json保存

--- create.py
+++ create.py
@@ -1,60 +1,64 @@
-from lxml import etree
-import json
+# 导入必要的模块
+from lxml import etree  # 用于XML解析
+import json  # 用于JSON序列化
+import html  # 用于HTML实体转义
+from Record_class import Article, Book, WWW, Inproceedings, Mastersthesis, Incollection, Proceedings, Phdthesis
 
-class Article:
-    def __init__(self, **kwargs):
-        self.title = kwargs.get('title', None)
-        self.authors = kwargs.get('authors', [])
-        self.journal = kwargs.get('journal', None)
-        self.year = kwargs.get('year', None)
-        self.volume = kwargs.get('volume', None)
-        self.number = kwargs.get('number', None)
-        self.pages = kwargs.get('pages', None)
-        self.month = kwargs.get('month', None)
-        self.publisher = kwargs.get('publisher', None)
-        self.booktitle = kwargs.get('booktitle', None)
-        self.editors = kwargs.get('editors', [])
-        self.url = kwargs.get('url', None)
-        self.ee = kwargs.get('ee', None)
-        self.cite = kwargs.get('cite', None)
-        self.cdrom = kwargs.get('cdrom', None)
-        self.crossref = kwargs.get('crossref', None)
-        self.publnr = kwargs.get('publnr', None)
-        self.note = kwargs.get('note', None)
-
-    def to_dict(self):
-        return {k: v for k, v in self.__dict__.items() if v is not None}
-
-def create_article_element(element):
-    article_info = {}
+# 创建记录元素的函数
+def create_record_element(element, tag):
+    record_info = {}  # 用于存储记录信息的字典
+    # 遍历子元素
     for child in element:
+        # 根据子元素的标签进行处理
         if child.tag == 'author':
-            article_info.setdefault('authors', []).append(child.text)
+            record_info.setdefault('authors', []).append(html.unescape(child.text))
+        elif child.tag == 'editor':
+            record_info.setdefault('editors', []).append(html.unescape(child.text))
         else:
-            article_info[child.tag] = child.text
-    return Article(**article_info)
+            record_info[child.tag] = child.text
+    # 根据记录类型返回相应的记录对象
+    match tag:
+        case "article": return Article(**record_info)
+        case "book": return Book(**record_info)
+        case "www": return WWW(**record_info)
+        case "inproceedings": return Inproceedings(**record_info)
+        case "mastersthesis": return Mastersthesis(**record_info)
+        case "incollection": return Incollection(**record_info)
+        case "proceedings": return Proceedings(**record_info)
+        case "phdthesis": return Phdthesis(**record_info)
 
-
-def read_articles_from_xml(file_path):
-    articles = []
-    context = etree.iterparse(file_path, events=('start', 'end'))
+# 从XML文件中读取记录
+def read_records_from_xml(file_path, record_types):
+    records = {record_type: [] for record_type in record_types}  # 初始化记录字典
+    context = etree.iterparse(file_path, events=('start', 'end'))  # 创建XML解析器上下文
+    current_record_type = None  # 当前记录类型初始化为空
+    # 遍历XML解析器上下文中的事件和元素
     for event, element in context:
-        if event == 'end' and element.tag == 'article':
-            article = create_article_element(element)
-            articles.append(article)
-            element.clear()
+        if event == 'start' and element.tag in record_types:
+            # 如果是起始事件且元素标签在记录类型列表中，则更新当前记录类型
+            current_record_type = element.tag
+        if event == 'end' and current_record_type is not None and element.tag == current_record_type:
+            # 如果是结束事件且当前记录类型不为空且元素标签与当前记录类型相同，则创建记录对象并添加到记录字典中
+            record = create_record_element(element, current_record_type)
+            records[current_record_type].append(record)
+            element.clear()  # 清除当前元素及其子元素
             while element.getprevious() is not None:
-                del element.getparent()[0]
-    del context
-    return articles
+                del element.getparent()[0]  # 删除当前元素的父节点的第一个子节点
+    del context  # 删除XML解析器上下文
+    return records  # 返回记录字典
 
+# 定义记录类型列表和XML文件路径
+record_types = ['article', 'book', 'www', 'inproceedings', 'mastersthesis', 'incollection', 'proceedings', 'phdthesis']
+file_path = 'dblp_deal.xml'
 
-# 读取 XML 文件并创建 Article 对象列表
-file_path = 'test.xml'
-articles = read_articles_from_xml(file_path)
+# 从XML文件中读取记录
+records = read_records_from_xml(file_path, record_types)
 
-# 打印所有 Article 对象的信息
-output_file = 'articles.json'
+# 将记录对象转换为字典
+for key, value in records.items():
+    records[key] = [item.to_dict() for item in value]
+
+# 将记录字典写入JSON文件
+output_file = 'Record.json'
 with open(output_file, 'w') as f:
-    articles_data = [article.to_dict() for article in articles]
-    json.dump(articles_data, f, indent=4)
+    json.dump(records, f, indent=4)  # 以缩进格式写入JSON文件
